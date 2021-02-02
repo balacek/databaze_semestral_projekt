f6658ea26394f48a6424fc7b5278151641668022
balacek <david.bali@seznam.cz>
2021-02-02 16:16:21 +0100

    fix

--- files/17_skript_analyticke_dotazy.sql
+++ files/17_skript_analyticke_dotazy.sql
@@ -1,3 +1,3 @@
 -- Seznam všech učitelů a jejich počet hodin i s celkovymi součty, které jsou v datu od 1.1.21
-select sum(r.pocetHodin), u.jmeno from rozvrhova_akce r join ucitel u on r.ucitel_id = u.id 
-where r.datum > to_date('01.01.21','DD.MM.RR') group by cube(r.pocetHodin, u.jmeno) order by r.jmeno;
+select sum(r.pocetHodin) as "Hodiny učitele", u.jmeno from rozvrhova_akce r join ucitel u on r.ucitel_id = u.id 
+where r.datum > to_date('01.01.21','DD.MM.RR') group by cube(r.pocetHodin, u.jmeno) order by u.jmeno;

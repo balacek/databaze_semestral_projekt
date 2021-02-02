6a63e78242869aaf6c11b352fbfb37c701e76c13
balacek <david.bali@seznam.cz>
2021-02-02 16:56:20 +0100

    analytické dotazy

--- files/17_skript_analyticke_dotazy.sql
+++ files/17_skript_analyticke_dotazy.sql
@@ -1,3 +1,5 @@
+SET AUTOTRACE ON;
+SET ECHO ON;
 -- Seznam všech učitelů a jejich počet hodin i s celkovymi součty, které jsou v datu od 1.1.21 pro predmět kde je učitel vyučující
 select sum(r.pocetHodin) as "Hodiny učitele", u.jmeno from rozvrhova_akce r join ucitel u on r.ucitel_id = u.id 
 where r.datum > to_date('01.01.21','DD.MM.RR') and r.predmet_id = u.id group by cube(r.pocetHodin, u.jmeno) order by u.jmeno;
